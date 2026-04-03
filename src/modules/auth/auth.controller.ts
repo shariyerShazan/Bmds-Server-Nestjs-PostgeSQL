244c2cfa623b6366c11b062a9ff845d90bab520a
Shariyer Shazan <shariyershazan1@gmail.com>
2026-04-03 06:24:35 +0600

    Update auth.controller.ts

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -48,8 +48,8 @@ export class AuthController {
     res.cookie('accessToken', result.access_token, {
       maxAge: 7 * 24 * 60 * 60 * 1000,
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'lax',
+        secure: true,  
+      sameSite: 'none',
       path: '/',
     });
 
